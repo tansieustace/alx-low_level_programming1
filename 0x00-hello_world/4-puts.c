8c97d76b0603b85c1042687971a5edb8e2ece91d
tansieustace <eustacetansi@gmail.com>
2022-09-09 04:32:22 -0700

    my task fourth commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,22 +1,13 @@
 #include <stdio.h>
 
-
 /**
- *
  * main - Entry point
  *
- * *
- *
  * Return: Always 0 (success)
- *
  */
-
 int main(void)
-
 {
-
-	puts("\"Programming is like building a multilingual puzzle");
-
+	puts("\ "programming is like building a multilingual puzzle");
 	return (0);
-
 }
+
