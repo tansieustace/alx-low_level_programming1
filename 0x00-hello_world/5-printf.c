1f1d08cceaf36e2df5d415ab5aa76b6d0b261f10
tansieustace <eustacetansi@gmail.com>
2022-09-08 21:00:13 -0700

    my task fifth commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *
+ * main-entry point for the program.
+ *
+ *
+ *
+ * Return: By default returns zero if no errors otherwise returns non-zero value.
+ *
+ */
+
+int main(void)
+
+{
+
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+
+	return (0);
+
+}
